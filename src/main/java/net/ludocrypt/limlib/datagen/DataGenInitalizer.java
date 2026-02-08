b60b0a727b7d45eb6b84db6386da135ba2e4ad0e
Waterpicker <waterpickerenternity@gmail.com>
2026-02-07 20:57:50 -0600

    Remove Test datagen

--- src/main/java/net/ludocrypt/limlib/datagen/DataGenInitalizer.java
+++ src/main/java/net/ludocrypt/limlib/datagen/DataGenInitalizer.java
@@ -42,40 +42,27 @@ public class DataGenInitalizer implements DataGeneratorEntrypoint {
 	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
 
 		var pack = fabricDataGenerator.createPack();
-		pack.addProvider(new FabricDataGenerator.Pack.RegistryDependentFactory<DataProvider>() {
+		pack.addProvider((FabricDataGenerator.Pack.RegistryDependentFactory<DataProvider>) (output, registriesFuture) -> new FabricDynamicRegistryProvider(output, registriesFuture) {
 			@Override
-			public DataProvider create(FabricDataOutput output, CompletableFuture<HolderLookup.Provider> registriesFuture) {
-				return new FabricDynamicRegistryProvider(output, registriesFuture) {
-					@Override
-					protected void configure(HolderLookup.Provider registries, Entries entries) {
-						entries.add(ResourceKey.create(LimLibRegistries.SKYBOX, ResourceLocation.withDefaultNamespace("overworld")), new TexturedSkybox(Limlib.id("textures/sky/yearning_canal")));
-						entries.add(ResourceKey.create(LimLibRegistries.DIMENSION_EFFECTS, ResourceLocation.withDefaultNamespace("overworld")), StaticDimensionEffects.EMPTY);
+			protected void configure(HolderLookup.Provider registries, Entries entries) {
+				registries.lookup(Registries.BIOME).flatMap(a -> a.get(Biomes.THE_VOID)).ifPresent(biome -> {
+					registries.lookup(Registries.DIMENSION_TYPE).flatMap(b -> b.get(BuiltinDimensionTypes.OVERWORLD)).ifPresent(dimension -> {
+						entries.add(DEBUG_KEY, new WorldPreset(
+							Map.of(LevelStem.OVERWORLD, new LevelStem(
+								dimension, new DebugNbtChunkGenerator(biome)
+							))
+						));
+					});
+				});
+			}
 
-						entries.add(ResourceKey.create(SoundEffects.SOUND_EFFECTS_KEY, ResourceLocation.withDefaultNamespace("overworld")), new SoundEffects(Optional.of(new StaticReverbEffect.Builder().setDecayTime(20.0F).build()),
-							Optional.empty(), Optional.empty()));
-
-						registries.lookup(Registries.BIOME).flatMap(a -> a.get(Biomes.THE_VOID)).ifPresent(biome -> {
-							registries.lookup(Registries.DIMENSION_TYPE).flatMap(b -> b.get(BuiltinDimensionTypes.OVERWORLD)).ifPresent(dimension -> {
-								entries.add(DEBUG_KEY, new WorldPreset(
-									Map.of(LevelStem.OVERWORLD, new LevelStem(
-										dimension, new DebugNbtChunkGenerator(biome)
-									))
-								));
-							});
-						});
-
-						entries.add(ResourceKey.create(LimLibRegistries.POST_EFFECT, ResourceLocation.withDefaultNamespace("overworld")), new StaticPostEffect(Limlib.id("yearning_canal")));
-					}
-
-					@Override
-					public String getName() {
-						return "Liminal Test";
-					}
-				};
+			@Override
+			public String getName() {
+				return "Liminal Test";
 			}
 		});
 
-		pack.addProvider(new FabricDataGenerator.Pack.RegistryDependentFactory<DataProvider>() {
+		pack.addProvider(new FabricDataGenerator.Pack.RegistryDependentFactory<>() {
 			@Override
 			public DataProvider create(FabricDataOutput output, CompletableFuture<HolderLookup.Provider> registriesFuture) {
 				return new FabricTagProvider<WorldPreset>(output, Registries.WORLD_PRESET, registriesFuture) {
