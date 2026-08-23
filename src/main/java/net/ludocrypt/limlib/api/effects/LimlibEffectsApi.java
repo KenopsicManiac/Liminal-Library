84810d9b216eaa18c7f81bbc611684522f965a68
KenopsicManiac <db318330@gmail.com>
2026-08-23 18:12:19 +1200

    Update the version of the mod

--- src/main/java/net/ludocrypt/limlib/api/effects/LimlibEffectsApi.java
+++ src/main/java/net/ludocrypt/limlib/api/effects/LimlibEffectsApi.java
@@ -9,24 +9,46 @@ import net.minecraft.sounds.Music;
 
 import java.util.Optional;
 
-//TODO Test API on debug world
+/**
+ * Client-sided api for sound effects, such as music, distortion filters and reverb filters
+ */
 public class LimlibEffectsApi {
 	protected static final SoundEffectStorage SOUND_STORAGE = new SoundEffectStorage();
 
+	/**
+	 * Gets the sound effects for a dimension
+	 * @param dimensionId dimension id
+	 * @return if found, the sound effects of the dimension
+	 */
 	public static Optional<SoundEffects> getEffects(ResourceLocation dimensionId) {
 		if (!SOUND_STORAGE.hasEntry(dimensionId)) return Optional.empty();
 
 		return Optional.of(SOUND_STORAGE.getEntry(dimensionId));
 	}
 
+	/**
+	 * Gets the distortion filter for a dimension
+	 * @param dimensionId dimension id
+	 * @return if found, the distortion filter of the dimension
+	 */
 	public static Optional<DistortionEffect> getDistortion(ResourceLocation dimensionId) {
 		return getEffects(dimensionId).flatMap(SoundEffects::distortion);
 	}
 
+	/**
+	 * Gets the reverb filter for a dimension
+	 * @param dimensionId dimension id
+	 * @return if found, the reverb filter of the dimension
+	 */
 	public static Optional<ReverbEffect> getReverb(ResourceLocation dimensionId) {
 		return getEffects(dimensionId).flatMap(SoundEffects::reverb);
 	}
 
+	/**
+	 * Gets the music for a dimension
+	 * @param dimensionId dimension id
+	 * @return if found, the music of the dimension
+	 */
 	public static Optional<Music> getMusic(ResourceLocation dimensionId) {
 		return getEffects(dimensionId).flatMap(SoundEffects::music);
 	}
